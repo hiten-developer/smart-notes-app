8b57a2c9990c3f4cb25f7b9d6d0ac16bf338cdda
Prajapati Hiten <prajapatihiten2510@gmail.com>
2026-03-26 10:29:11 +0530

    backend server and mongo connection setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const app = require('./app')
+const { connectDB } = require('./config/db')
 
 const PORT = process.env.PORT
 
-app.listen(PORT,() => {
-    console.log("Server is Started...");
-    
-})
+const startServer = async () => {
+    await connectDB()
+
+    app.listen(PORT, () => {
+        console.log("Server is Started...");
+
+    })
+}
+startServer()
